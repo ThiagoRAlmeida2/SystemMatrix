0468168b2c71af8a8b8ec5ef976ca70e6ec8e7d2
ThiagoRAlmeida2 <129293006+ThiagoRAlmeida2@users.noreply.github.com>
2024-05-03 22:05:35 -0300

    Delete Banco directory

--- Banco/src/main/java/execoes/SaldoInsuficienteException.java
+++
@@ -1,8 +0,0 @@
-package execoes;
-
-public class SaldoInsuficienteException extends Exception{
-
-    public SaldoInsuficienteException(){
-        super("Saldo  insuficiente para esta operação!");
-    }
-}
